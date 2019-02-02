2c6781d9e6ddc4eda088025556508094e2c418a9
Mohammed Aljasser <justmo59@gmail.com>
2019-02-02 12:20:14 +0300

    delete bug

--- lib/pages/project.dart
+++ lib/pages/project.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 class ProjectPage extends StatelessWidget {
   final String title;
-  final String imageUrl ;
+  final String imageUrl;
 
-  ProjectPage(this.title , this.imageUrl)
+  ProjectPage(this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: new Text(title),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.network(imageUrl),
-            RaisedButton(
-              child: Text('Delete'),
-              onPressed: () => Navigator.pop(context, true),
-            )
-          ],
-        )));
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: new Text(title),
+          ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.network(imageUrl),
+              RaisedButton(
+                child: Text('Delete'),
+                onPressed: () => Navigator.pop(context, true),
+              )
+            ],
+          ))),
+    );
   }
 }
