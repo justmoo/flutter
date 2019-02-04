97d43cc3c7fb11ecd6091c333ee8ffbda446431d
Mohammed Aljasser <justmo59@gmail.com>
2019-02-04 17:07:06 +0300

    auth file

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart';
 import './projects.dart';
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
 
-class AuthPage extends StatelessWidget {
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: new Text('Auth Page'),
+        title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child:  Text('The password ?'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/'
-             );
-          },
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'E-Mail'),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _emailValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: Text('LOGIN'),
+              onPressed: () {
+                print(_emailValue);
+                print(_passwordValue);
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ],
         ),
       ),
     );
-    
   }
 }
+
